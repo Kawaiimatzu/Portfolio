0a39d5180688f9638a58332a3914e8d862af9861
Kawaiimatzu <arvhieabarquez@gmail.com>
2026-05-28 05:20:48 +0800

    update

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Luxury Portfolio Website Loaded");
+function openPreview(src){
+
+  const preview = document.getElementById("preview");
+  const previewImg = document.getElementById("preview-img");
+
+  preview.classList.add("active");
+
+  previewImg.src = src;
+
+}
+
+function closePreview(){
+
+  document.getElementById("preview")
+  .classList.remove("active");
+
+}
