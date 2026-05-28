4f674aa9873886daa54fdca338733c8c3a659c53
Kawaiimatzu <arvhieabarquez@gmail.com>
2026-05-29 02:35:51 +0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+console.log("Luxury Portfolio Website Loaded");
+
+/* =========================
+   IMAGE PREVIEW
+========================= */
+
 function openPreview(src){
 
   const preview = document.getElementById("preview");
@@ -11,7 +17,60 @@ function openPreview(src){
 
 function closePreview(){
 
-  document.getElementById("preview")
+  document
+  .getElementById("preview")
   .classList.remove("active");
 
 }
+
+/* =========================
+   MOUSE PARTICLES
+========================= */
+
+document.addEventListener("mousemove",(e)=>{
+
+  const particle =
+  document.createElement("div");
+
+  particle.className =
+  "mouse-particle";
+
+  particle.style.left =
+  e.clientX + "px";
+
+  particle.style.top =
+  e.clientY + "px";
+
+  document.body.appendChild(particle);
+
+  setTimeout(()=>{
+
+    particle.remove();
+
+  },800);
+
+});
+
+let lastScroll = 0;
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+  const currentScroll = window.pageYOffset;
+
+  if(currentScroll > lastScroll){
+
+    /* SCROLL DOWN */
+    navbar.style.transform = "translateY(-120%)";
+
+  }else{
+
+    /* SCROLL UP */
+    navbar.style.transform = "translateY(0)";
+
+  }
+
+  lastScroll = currentScroll;
+
+});
